9a34e67cac6a3a0403178eacd3cf587d59cb46a5
Léo Capdevielle <leo.capde64@gmail.com>
2021-10-22 15:46:27 +0200

    Create the fecth POST to send the command

--- front-end/js/shopping.js
+++ front-end/js/shopping.js
@@ -1,6 +1,7 @@
 // Récupéreation des données
 
 const fetchPromise = "http://localhost:3000/api/teddies";
+const fetchPost = "http://localhost:3000/api/teddies/order";
 
 // Convertisseur des prix en €
 
@@ -14,6 +15,21 @@ const euro = new Intl.NumberFormat('fr-FR', {
 
 document.getElementById("shopping_quantity").innerHTML += ` <span class="fw-bold ">(${localStorage.length})</span>`;
 
+// Création des objets
+
+let products = new Array();
+let contact;
+
+function createContact() {
+  contact = {
+    firstName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    address: document.getElementById("address").value,
+    city: document.getElementById("city").value,
+    email: document.getElementById("email").value
+  }
+};
+
 // Création de la liste des produits ajoutés au panier
 
 function createShoppingList() {
@@ -24,7 +40,6 @@ function createShoppingList() {
     .then((data) => {
       const shoppingList = document.getElementById("shopping_list");
       let totalShopping = 0;
-      // let productQuantity = 0;
 
       for (let obj of data) {
         for (let key in localStorage) {
@@ -69,10 +84,26 @@ function createShoppingList() {
             `;
 
             totalShopping += obj.price * productQuantity;
+
+            // Ajout du contenu dans l'objet products
+
+            products.push(
+              obj._id
+            //   {
+            //   _id: obj._id,
+            //   name: obj.name,
+            //   price: obj.price,
+            //   imageUrl: obj.imageUrl,
+            //   description: obj.description,
+            //   colors: obj.colors,
+            //   quantity: productQuantity,
+            //   total_price: obj.price * productQuantity
+            // }
+            );
           } 
         }
       }
-      if (shoppingList.hasChildNodes() == false) {
+      if (!shoppingList.hasChildNodes()) {
         shoppingList.innerHTML = 
         `
         <p class="mt-5 text-center">Votre panier est actuellement vide ...</p>
@@ -112,40 +143,81 @@ validationButton.addEventListener('click', (event) => {
               <h6 class="text-secondary mb-0 text-start">
                   <label for="lastName">Nom :</label>
               </h6>
-              <input class="w-100" type="text" id="lastName" name="user_lastName" required>
+              <input class="w-100" type="text" id="lastName" name="lastName" required>
           </div>
           <div class="mt-3">
               <h6 class="text-secondary mb-0 text-start">
                   <label for="firstName">Prénom :</label>
               </h6>
-              <input class="w-100" type="text" id="firstName" name="user_firstName" required>
+              <input class="w-100" type="text" id="firstName" name="firstName" required>
           </div>
           <div class="mt-3">
               <h6 class="text-secondary mb-0 text-start">
                   <label for="address">Adresse :</label>
               </h6>
-              <input class="w-100" type="text" id="address" name="user_address" required>
+              <input class="w-100" type="text" id="address" name="address" required>
           </div>
           <div class="mt-3">
               <h6 class="text-secondary mb-0 text-start">
                   <label for="city">Ville :</label>
               </h6>
-              <input class="w-100" type="text" id="city" name="user_city" required>
+              <input class="w-100" type="text" id="city" name="city" required>
           </div>
           <div class="mt-3">
               <h6 class="text-secondary mb-0 text-start">
                   <label for="email">Email :</label>
               </h6>
-              <input class="w-100" type="email" id="email" name="user_email" required>
+              <input class="w-100" type="email" id="email" name="email" required>
           </div>
           <div class="mt-4 mb-3">
               <button id="confirmation" class="btn btn-success" type="submit">Confirmer la commande</button>
           </div>
       </form>
       `;
+
+      // Création de l'objet contact
+
+      const confirmationButton = document.getElementById("validation_form");
+      confirmationButton.addEventListener('submit', (event) => {
+        event.preventDefault(); // Permet de ne pas envoyer le formulaire
+        if (confirm("Êtes-vous sûr de vouloir envoyer le formulaire ?")) {
+        createContact();
+        sendCommand();
+        }
+      });
     }
   }
 })
+
+// Envoie de la commande 
+
+function sendCommand() {
+  fetch(fetchPost, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      contact, products
+    })
+  })
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    // localStorage.clear();
+    // localStorage.setItem("products", products);
+    // localStorage.setItem("contact", contact);
+
+    // renvoyer vers URL/orderId
+    // location.href = "validation.html?id="+data.orderId;
+
+    console.log(data.orderId);
+  })
+  .catch((err) => {
+    console.log("Erreur fonction sendCommand()");
+  });
+}
 
 // Vider le panier
 
